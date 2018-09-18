cc4a640d300cd7e8eef953570b5c4f63ff88e54f
Chris Ashworth <chris@cashworth.net>
2018-09-18 08:21:29 +0200

    Initial commit

---
+++ Source/UEBOIDS/Public/UEBOIDS.h
@@ -0,0 +1,18 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(UEBOIDS, Log, All);
+
+
+class FUEBOIDSModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
